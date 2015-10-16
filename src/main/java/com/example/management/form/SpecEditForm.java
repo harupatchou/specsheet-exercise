46c424bc0bbf13b8e8fab5413f8f05628d4930e2
kurosawa <yu_kurosawa@localhost>
2015-10-16 18:11:30 +0900

    スペックシート登録jsp作成

---
+++ src/main/java/com/example/management/form/SpecEditForm.java
@@ -0,0 +1,14 @@
+package com.example.management.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpecEditForm {
+	
+	private String a;
+
+}
