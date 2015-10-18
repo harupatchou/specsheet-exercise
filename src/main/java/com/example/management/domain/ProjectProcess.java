bda019f1592a9a32c1dabf33fdc4833112a63dc0
Takayoshi Okamoto <takayoshi.okamoto@rakus.co.jp>
2015-10-19 08:56:42 +0900

    domain

---
+++ src/main/java/com/example/management/domain/ProjectProcess.java
@@ -0,0 +1,22 @@
+package com.example.management.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * プロジェクト担当工程.
+ * @author okamoto
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProjectProcess {
+	/**スタッフID */
+	private String staffId;
+	/**プロジェクトNo */
+	private Integer projectNo;
+	/**担当工程ID */
+	private Integer processId;
+}
