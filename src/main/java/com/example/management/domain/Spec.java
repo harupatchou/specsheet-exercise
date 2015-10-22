ea374cb36b7d85aa94ea4b8cba9b015bbdbbaa74
kurosawa <yu_kurosawa@localhost>
2015-10-22 09:33:16 +0900

    sql

--- src/main/java/com/example/management/domain/Spec.java
+++ src/main/java/com/example/management/domain/Spec.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.data.annotation.Transient;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
