0666a568ca94a4e2294a811f52b6e0b7a9db7d1e
Takayoshi Okamoto <takayoshi.okamoto@rakus.co.jp>
2015-10-26 11:47:26 +0900

    プルのためと、資格登録

--- src/main/java/com/example/management/repository/SpecRegistRepository.java
+++ src/main/java/com/example/management/repository/SpecRegistRepository.java
@@ -1,9 +1,13 @@
 package com.example.management.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.management.form.SpecRegistLicenseForm;
 
 @Transactional
 @Repository
@@ -11,4 +15,18 @@ public class SpecRegistRepository {
 	@Autowired
 	private NamedParameterJdbcTemplate jdbcTemplate;
 
+	
+	/**
+	 * スペックシート登録（資格）
+	 * @author okamoto
+	 * @param form
+	 */
+	public void insertUsersLicenseByStaffId(SpecRegistLicenseForm form){
+		SqlParameterSource param = new BeanPropertySqlParameterSource(form);
+		jdbcTemplate.update(
+				"INSERT INTO users_license (staff_id , name , acquire_date) "
+				+ "　VALUES ( :staffId , :name , :acquireDate); ",
+				param);
+	}
+	
 }
