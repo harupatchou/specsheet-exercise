53963829e764dd5206e0238b54c4022e5180c793
Takayoshi Okamoto <takayoshi.okamoto@rakus.co.jp>
2015-10-26 11:56:25 +0900

    いったんサービス追加

---
+++ src/main/java/com/example/management/service/SpecRegistService.java
@@ -0,0 +1,14 @@
+package com.example.management.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * スペックシート登録を行うサービス.
+ * @author okamoto
+ *
+ */
+@Service
+public class SpecRegistService {
+
+}
