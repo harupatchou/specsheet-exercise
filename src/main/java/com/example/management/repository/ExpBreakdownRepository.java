39a1fdaaad73b639e4d10682c1fb47766ee92ef7
Takayuki Honma <takayuki.honma@rakus.co.jp>
2015-10-27 10:15:55 +0900

    pullする

--- src/main/java/com/example/management/repository/ExpBreakdownRepository.java
+++ src/main/java/com/example/management/repository/ExpBreakdownRepository.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.management.form.SpecForm;
 import com.example.management.page.ExpBreakdownPage;
 
 @Transactional
@@ -49,34 +47,5 @@ public class ExpBreakdownRepository {
 //			return null;
 //		}
 	}
-	
-	/**
-	 * 経験内訳の登録
-	 * @param form
-	 */
-	public void insertBreakdown(SpecForm form) {
-		SqlParameterSource param = new BeanPropertySqlParameterSource(form);
-		if (form.getServerNetworkExpMonth() != 0 || form.getServerNetworkExpYear() != 0) {
-			form.setMonthOfExp(form.getServerNetworkExpMonth() + form.getServerNetworkExpYear() * 12);
-			param = new BeanPropertySqlParameterSource(form);
-			jdbcTemplate.update("INSERT INTO exp_breakdown VALUES (:staffId, '1', :monthOfExp)", param);
-		}
-		if (form.getDevelopmentExpMonth() != 0 || form.getDevelopmentExpYear() != 0) {
-			form.setMonthOfExp(form.getDevelopmentExpMonth() + form.getDevelopmentExpYear() * 12);
-			param = new BeanPropertySqlParameterSource(form);
-			jdbcTemplate.update("INSERT INTO exp_breakdown VALUES (:staffId, '2', :monthOfExp)", param);
-		}
-		if (form.getSeExpMonth() != 0 || form.getSeExpYear() != 0) {
-			form.setMonthOfExp(form.getSeExpMonth() + form.getSeExpYear() * 12);
-			param = new BeanPropertySqlParameterSource(form);
-			jdbcTemplate.update("INSERT INTO exp_breakdown VALUES (:staffId, '3', :monthOfExp)", param);
-		}
-		if (form.getPgOperatorExpMonth() != 0 || form.getPgOperatorExpYear() != 0) {
-			form.setMonthOfExp(form.getPgOperatorExpMonth() + form.getPgOperatorExpYear() * 12);
-			param = new BeanPropertySqlParameterSource(form);
-			jdbcTemplate.update("INSERT INTO exp_breakdown VALUES (:staffId, '4', :monthOfExp)", param);
-		}
-	}
-	
 
 }
--- src/main/java/com/example/management/service/SpecRegistService.java
+++ src/main/java/com/example/management/service/SpecRegistService.java
@@ -1,6 +1,10 @@
 package com.example.management.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.management.form.SpecForm;
+import com.example.management.repository.SpecRegistRepository;
 
 /**
  * スペックシート登録を行うサービス.
@@ -9,6 +13,16 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SpecRegistService {
+	@Autowired
+	SpecRegistRepository specRegistRepository;
+	
+	/**
+	 * 経験内訳の登録
+	 * @param form
+	 */
+	public void insertBreakdown(SpecForm form) {
+		specRegistRepository.insertBreakdown(form);
+	}
 	
 
 }
